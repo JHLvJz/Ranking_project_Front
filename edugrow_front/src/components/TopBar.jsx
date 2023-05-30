f79077dc11c8c506235aa75dd27eae09b1f992d3
JHLvJz <jhsu1619@gmail.com>
2023-05-30 23:49:28 +0900

    입력값 일치하는 검색 목록기능_형식 고민중

--- edugrow_front/src/components/TopBar.jsx
+++ edugrow_front/src/components/TopBar.jsx
@@ -7,20 +7,68 @@ import {
   UserImg,
 } from "../styles/Home";
 import { TopContainer } from "../styles/layout";
+import x from "../SampleData";
+import styled from "styled-components";
+import buildingList from "../SampleData";
 
 function TopBar() {
+  let matchedList = [];
+
+  /* 검색기능 */
+  function SearchingWord(userInput) {
+    if (buildingList && userInput.length !== 0) {
+      matchedList = buildingList.filter((building) =>
+        building["name"].includes(userInput)
+      );
+    }
+
+    // buildingList?.map((building) => {
+    //   //console.log(element["name"], "얍");
+    //   if (building["name"].includes(userInput) && userInput.length !== 0) {
+    //     matchedList.push(building["name"]);
+    //   }
+    // });
+
+    console.log(matchedList, "-----");
+  }
+
   return (
-    <TopContainer>
-      <MypageBox>
-        <UserImg src="/img/user.png" />
-      </MypageBox>
-      <SearchBox>
-        <ListLogo src="img/list.png" />
-        <SearchInput placeholder="공부할 장소 검색" />
-        <SearchLogo src="img/search.png" />
-      </SearchBox>
-    </TopContainer>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <TopContainer>
+        <MypageBox>
+          <UserImg src="/img/user.png" />
+        </MypageBox>
+        <SearchBox>
+          <ListLogo src="img/list.png" />
+          <div>
+            <SearchInput
+              placeholder="공부할 장소 검색"
+              onChange={(e) => SearchingWord(e.target.value)}
+            />
+            {buildingList?.map((building) => (
+              <div key={building["id"]}>{building["name"]}</div>
+            ))}
+          </div>
+          <SearchLogo src="img/search.png" />
+        </SearchBox>
+      </TopContainer>
+    </div>
   );
 }
+
+/* 검색목록 */
+const SearchList = styled.ul`
+  width: 100%;
+  background-color: blue;
+  z-index: 10;
+  /* display: ${({ isExist }) => (isExist ? "block" : "none")}; */
+`;
+
+const ListItem = styled.li`
+  width: 200px;
+  height: 20px;
+  z-index: 10;
+  background-color: red;
+`;
 
 export default TopBar;
--- edugrow_front/src/pages/Home.jsx
+++ edugrow_front/src/pages/Home.jsx
@@ -2,39 +2,7 @@ import { GoogleMap, Marker, LoadScript, MarkerF } from "@react-google-maps/api";
 import { memo, useCallback, useMemo } from "react";
 import React from "react";
 import TopBar from "../components/TopBar";
-
-const buildingList = [
-  {
-    id: 1,
-    name: "운초우선교육관",
-    lat: 37.591584,
-    lng: 127.034381,
-  },
-  {
-    id: 2,
-    name: "백주년기념삼성관",
-    lat: 37.589553,
-    lng: 127.034358,
-  },
-  {
-    id: 3,
-    name: "중앙도서관",
-    lat: 37.590814,
-    lng: 127.034115,
-  },
-  {
-    id: 4,
-    name: "하나스퀘어",
-    lat: 37.584919,
-    lng: 127.025963,
-  },
-  {
-    id: 5,
-    name: "과학도서관",
-    lat: 37.584631,
-    lng: 127.026541,
-  },
-];
+import buildingList from "../SampleData";
 
 function Home() {
   const onLoad = (marker) => {
@@ -65,7 +33,6 @@ function Home() {
           options={mapOptions}
         >
           <TopBar />
-          <p>브랜치 테스트</p>
           {buildingList?.map((building) => (
             <MarkerF
               key={building["id"]}

db0479d7c6b9a47471602514bda43d86fc428545
정윤영[ 학부재학 / 교육학과 ] <vandaj@korea.ac.kr>
2023-06-01 18:44:08 +0900

    윤영이 브랜치 테스트

--- edugrow_front/src/App.js
+++ edugrow_front/src/App.js
@@ -1,4 +1,5 @@
 import Home from "./pages/Home";
+import Login from "../src/pages/Login"
 import "./App.css";
 import { Layout } from "./styles/layout";
 
@@ -6,7 +7,9 @@ function App() {
   return (
     <Layout>
       <div className="App">
-        <Home />
+        {/* <Home /> */}
+        <Login/>
+
       </div>
     </Layout>
   );
---
+++ edugrow_front/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return(
+    <div>윤영
+      <b>떡</b>
+      </div>
+    
+  )
+}
+
+export default Login;
